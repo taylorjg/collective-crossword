6b8d90691ff671b5bda19960b78274f13518c101
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-17 18:38:28 +0000

    Allow Telegraph crossword id to be entered

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,4 +1,5 @@
-import { Button, Container, CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { Button, Container, CircularProgress, TextField } from "@mui/material";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -81,28 +82,67 @@ Crossword.propTypes = {
   onAddCrossword: PropTypes.func.isRequired,
 };
 
+const Crossword2 = ({ onAddCrossword, useHookFn, label, exampleId }) => {
+  const [id, setId] = useState("");
+  const [idToUse, setIdToUse] = useState("");
+
+  const crosswordResponse = useHookFn(idToUse);
+
+  const { crossword, isLoading, isError, error, exists } = crosswordResponse;
+
+  const handleFetchCrossword = () => {
+    setIdToUse(id);
+  };
+
+  const handleAddCrossword = () => {
+    onAddCrossword(crossword);
+  };
+
+  return (
+    <StyledBox>
+      <StyledBoxContent showContent={true}>
+        <div>{label}</div>
+        <TextField
+          label="Crossword ID"
+          placeholder={`e.g. ${exampleId}`}
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          variant="standard"
+          small
+        />
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={handleFetchCrossword}
+          disabled={!id || idToUse}
+        >
+          Fetch Crossword
+        </Button>
+        <div>Puzzle Title: {crossword?.title ?? ""}</div>
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={handleAddCrossword}
+          disabled={isLoading || exists === true}
+        >
+          Add Crossword
+        </Button>
+      </StyledBoxContent>
+      {isError && <Error error={error} />}
+    </StyledBox>
+  );
+};
+
+Crossword2.propTypes = {
+  onAddCrossword: PropTypes.func.isRequired,
+  useHookFn: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  exampleId: PropTypes.number.isRequired,
+};
+
 export const AdminPage = () => {
   const privateEyeCrosswordResponse = usePrivateEyeCurrentCrossword();
   console.log({ privateEyeCrossword: privateEyeCrosswordResponse });
-
-  const theDailyTelegraphCrypticCrosswordResponse =
-    useTheDailyTelegraphCrypticCrosswordById(31769);
-  console.log({
-    theDailyTelegraphCrypticCrossword:
-      theDailyTelegraphCrypticCrosswordResponse,
-  });
-
-  const theDailyTelegraphPrizeCrypticResponse =
-    useTheDailyTelegraphPrizeCrypticById(31711);
-  console.log({
-    theDailyTelegraphPrizeCryptic: theDailyTelegraphPrizeCrypticResponse,
-  });
-
-  const theSundayTelegraphPrizrCrypticResponse =
-    useTheSundayTelegraphPrizeCrypticById(31712);
-  console.log({
-    theSundayTelegraphPrizrCryptic: theSundayTelegraphPrizrCrypticResponse,
-  });
 
   const onAddCrossword = (crossword) => {
     addCrossword(crossword);
@@ -118,17 +158,26 @@ export const AdminPage = () => {
         crosswordResponse={privateEyeCrosswordResponse}
         onAddCrossword={onAddCrossword}
       />
-      <Crossword
-        crosswordResponse={theDailyTelegraphCrypticCrosswordResponse}
+
+      <Crossword2
         onAddCrossword={onAddCrossword}
+        useHookFn={useTheDailyTelegraphCrypticCrosswordById}
+        label="The Daily Telegraph Cryptic Crossword"
+        exampleId={31769}
       />
-      <Crossword
-        crosswordResponse={theDailyTelegraphPrizeCrypticResponse}
+
+      <Crossword2
         onAddCrossword={onAddCrossword}
+        useHookFn={useTheDailyTelegraphPrizeCrypticById}
+        label="The Daily Telegraph Prize Cryptic"
+        exampleId={31711}
       />
-      <Crossword
-        crosswordResponse={theSundayTelegraphPrizrCrypticResponse}
+
+      <Crossword2
         onAddCrossword={onAddCrossword}
+        useHookFn={useTheSundayTelegraphPrizeCrypticById}
+        label="The Sunday Telegraph Prize Cryptic"
+        exampleId={31712}
       />
     </Container>
   );
--- src/pages/CrosswordPage/CrosswordPage.jsx
+++ src/pages/CrosswordPage/CrosswordPage.jsx
@@ -26,6 +26,12 @@ export const CrosswordPage = () => {
     invokeGetCrossword();
   }, [id]);
 
+  const transformLine = (line) => {
+    const charsIn = Array.from(line);
+    const charsOut = charsIn.map((ch) => (ch === "." ? "\u2591" : "\u2588"));
+    return charsOut.join("");
+  };
+
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
       <div style={{ marginBottom: "1rem" }}>
@@ -35,6 +41,18 @@ export const CrosswordPage = () => {
       <div>id: {id}</div>
       <div>crossword.title: {crossword?.title}</div>
       <div>errorMessage: {errorMessage}</div>
+
+      {crossword && (
+        <>
+          <pre>
+            {crossword.grid.map((line, index) => (
+              <div key={index}>{transformLine(line)}</div>
+            ))}
+          </pre>
+          <br />
+          <pre>{JSON.stringify(crossword, null, 2)}</pre>
+        </>
+      )}
     </Container>
   );
 };
